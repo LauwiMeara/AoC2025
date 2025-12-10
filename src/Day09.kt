ae6b1cfca63cbbd4a815dda6e1138c689966981b
Lauwi Meara <laurasusanne@live.nl>
2025-12-10 06:18:31 +0100

    Add day 9.2 inefficient method

--- src/Day09.kt
+++ src/Day09.kt
@@ -21,7 +21,53 @@ fun main() {
         return getRectangleAreas(input).maxOf { it.value }
     }
 
+    fun addToPositionsSet(
+        input: List<Grid2D.Position>,
+        positionsSet: MutableSet<Grid2D.Position>
+    ) {
+        val yRangesForX = input.groupBy { it.x }
+            .map { Pair(it.key, it.value.minOf { position -> position.y }..it.value.maxOf { position -> position.y }) }
+        val xRangesForY = input.groupBy { it.y }
+            .map { Pair(it.key, it.value.minOf { position -> position.x }..it.value.maxOf { position -> position.x }) }
+        for (xWithYRange in yRangesForX) {
+            for (y in xWithYRange.second) {
+                positionsSet.add(Grid2D.Position(xWithYRange.first, y))
+            }
+        }
+        for (yWithXRange in xRangesForY) {
+            for (x in yWithXRange.second) {
+                positionsSet.add(Grid2D.Position(x, yWithXRange.first))
+            }
+        }
+    }
+
     fun part2(input: List<Grid2D.Position>): Long {
+        val positionsSet = mutableSetOf<Grid2D.Position>()
+        addToPositionsSet(input, positionsSet) // add outline
+        addToPositionsSet(positionsSet.toList(), positionsSet) // fill shape
+
+        val areas = getRectangleAreas(input).toList().sortedByDescending { it.second }
+        for (area in areas) {
+            val minX = area.first.toList().minOf{it.x}
+            val maxX = area.first.toList().maxOf{it.x}
+            val minY = area.first.toList().minOf{it.y}
+            val maxY = area.first.toList().maxOf{it.y}
+            var areaIsOutsideOfTiles = false
+            for (x in minX..maxX) {
+                if (areaIsOutsideOfTiles) break
+
+                for (y in minY..maxY) {
+                    if (!positionsSet.contains(Grid2D.Position(x, y))) {
+                        areaIsOutsideOfTiles = true
+                        break
+                    }
+                }
+            }
+
+            if (!areaIsOutsideOfTiles) {
+                return area.second
+            }
+        }
         return 0
     }
 
