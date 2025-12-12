0d0c5b5f639db32bd02bbc98c2ed2505fcc0aedd
Lauwi Meara <laurasusanne@live.nl>
2025-12-12 07:07:23 +0100

    Add day 11.2 other inefficient method

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,3 +1,7 @@
+import java.util.*
+
+data class Path(val nodes: List<String>, val visited: Set<String>)
+
 const val START_SERVER_PART_1 = "you"
 const val START_SERVER_PART_2 = "svr"
 const val DAC_SERVER = "dac"
@@ -5,28 +9,27 @@ const val FFT_SERVER = "fft"
 const val END_SERVER = "out"
 
 fun main() {
-    fun getPaths(input: Map<String, List<String>>, start: String): List<List<String>> {
-        val paths = mutableListOf(listOf(start))
+    fun getPaths(input: Map<String, List<String>>, start: String, end: String): List<List<String>> {
+        val paths = PriorityQueue<Path>(
+            compareBy { it.nodes.size }
+        )
+        paths.add(Path(nodes = listOf(start), visited = setOf(start)))
         val endedPaths = mutableListOf<List<String>>()
 
         while (paths.size != 0) {
-            println(paths.size)
-            println(endedPaths.size)
+            val currentPath = paths.poll()
 
-            val currentPath = paths.maxBy{it.size}
-            paths.remove(currentPath)
-
-            val nextServers = input[currentPath.last()] ?: emptyList()
+            val nextServers = input[currentPath.nodes.last()] ?: emptyList()
             for (nextServer in nextServers) {
                 // Don't circle through the paths.
-                if (currentPath.contains(nextServer)) continue
+                if (nextServer in currentPath.visited) continue
                 // Add to endedPaths if the end is reached.
-                if (nextServer == END_SERVER) {
-                    endedPaths.add(currentPath + END_SERVER)
+                if (nextServer == end) {
+                    endedPaths.add(currentPath.nodes + end)
                     continue
                 }
                 // Add the next server to the path.
-                paths.add(currentPath + nextServer)
+                paths.add(Path(nodes = currentPath.nodes + nextServer, visited = currentPath.visited + nextServer))
             }
         }
 
@@ -34,13 +37,25 @@ fun main() {
     }
 
     fun part1(input: Map<String, List<String>>): Long {
-        return getPaths(input, START_SERVER_PART_1).size.toLong()
+        return getPaths(input, START_SERVER_PART_1, END_SERVER).size.toLong()
     }
 
     fun part2(input: Map<String, List<String>>): Long {
-        return getPaths(input, START_SERVER_PART_2)
-            .filter{it.contains(DAC_SERVER) && it.contains(FFT_SERVER)}
-            .size.toLong()
+        // The possible paths are svr -> dac -> fft -> out and svr -> fft -> dac -> out.
+        // So the answer is the possible paths between (svr -> dac * dac -> fft * fft -> out) + (svr -> fft * fft -> dac * dac -> out)
+        val pathsSvrToDac = getPaths(input, START_SERVER_PART_2, DAC_SERVER).size.toLong()
+        println(pathsSvrToDac)
+        val pathsDacToFft = getPaths(input, DAC_SERVER, FFT_SERVER).size
+        println(pathsDacToFft)
+        val pathsFftToOut = getPaths(input, FFT_SERVER, END_SERVER).size
+        println(pathsFftToOut)
+        val pathsSvrToFft = getPaths(input, START_SERVER_PART_2, FFT_SERVER).size.toLong()
+        println(pathsSvrToFft)
+        val pathsFftToDac = getPaths(input, FFT_SERVER, DAC_SERVER).size
+        println(pathsFftToDac)
+        val pathsDacToOut = getPaths(input, DAC_SERVER, END_SERVER).size
+        println(pathsDacToOut)
+        return (pathsSvrToDac * pathsDacToFft * pathsFftToOut) + (pathsSvrToFft * pathsFftToDac * pathsDacToOut)
     }
 
     val input = readInputAsStrings("Day11")
